6f9a339961521477ecc68bc874cdab26ee503888
StanYanakiev <stan.yanakiev1@gmail.com>
2018-10-31 16:46:40 -0700

    SignUp modif

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,9 +9,23 @@ const SignUpPage = () =>
     <SignUpForm />
   </div>
 
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
 class SignUpForm extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { ...INITIAL_STATE };
   }
 
   onSubmit = (event) => {
@@ -19,9 +33,45 @@ class SignUpForm extends Component {
   }
 
   render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+
     return (
       <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event => this.setState(byPropKey('username', event.target.value))}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          value={email}
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={passwordOne}
+          onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button type="submit">
+          Sign Up
+        </button>
 
+        {error && <p>{error.message}</p>}
       </form>
     );
   }
