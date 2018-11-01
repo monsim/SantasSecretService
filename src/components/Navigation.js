e285e919ad2782e5ffefb59c6a2b269c60c300d3
Priscilla Ng <priscillasrng@gmail.com>
2018-11-01 12:07:40 -0700

    finished Session Handling in React with Higher Order Components. authentication flow works; on Password Reset and Password Change now

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import AuthUserContext from './AuthUserContext';
 import SignOutButton from './SignOut';
 import * as routes from '../constants/routes';
 
-const Navigation = ({ authUser }) =>
-  <div>
-    { authUser
-        ? <NavigationAuth />
-        : <NavigationNonAuth />
+const Navigation = () =>
+  <AuthUserContext.Consumer>
+    {authUser => authUser
+      ? <NavigationAuth />
+      : <NavigationNonAuth />
     }
-  </div>
+  </AuthUserContext.Consumer>
+
 
 const NavigationAuth = () =>
   <ul>
