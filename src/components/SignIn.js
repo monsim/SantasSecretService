c31717f2d30a7e51ec3457f84daef6822b044deb
monsim <monsi5pen@gmail.com>
2018-11-01 20:49:25 -0700

    until Authentication in React, Firebase and Redux

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import { SignUpLink } from './SignUp';
+import { PasswordForgetLink } from './PasswordForget';
 import { auth } from '../firebase';
 import * as routes from '../constants/routes';
 
@@ -9,6 +10,7 @@ const SignInPage = ({ history }) =>
   <div>
     <h1>SignIn</h1>
     <SignInForm history={history} />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app';
+import 'firebase/database';
 import 'firebase/auth';
 
 const config = {
@@ -14,8 +15,10 @@ if (!firebase.apps.length) {
     firebase.initializeApp(config);
 }
 
+const db = firebase.database();
 const auth = firebase.auth();
 
 export {
+    db,
     auth,
 };
