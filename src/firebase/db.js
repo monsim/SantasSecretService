a753e6597653e48895f52b06051c06c1befc73da
monsim <monsi5pen@gmail.com>
2018-11-13 00:58:01 -0800

    successfully retrieving only the first element in the wishlist

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -43,27 +43,60 @@ export function getWishlist(userID) {
         console.log('within promise line 43')
         console.log(userID)
         var groups = db.ref(`/users/${userID}/wishlist`);
-       
+
         console.log('before snapshot line 47')
         //call
         var outer = [];
-        helper(groups).then(function(result){
+        helper(groups).then(function (result) {
             console.log('within inner promise line 51')
             console.log('line 52: ' + result)
             outer = result;
             console.log('end inner promise line 54')
+            console.log('outer line 58 db ' + outer)
+            resolve(outer);
+            console.log('last line of promise line 59')
         })
         // console.log(list)
-        console.log('outer line 58 db ' + outer)
-        resolve(outer);
-        console.log('last line of promise line 59')
+        // resolve(outer);
+        // return promise;
     });
     console.log('after promise line 61')
     return promise;
 }
 
 export function helper(groups) {
-    var promise = new Promise(function (resolve, reject){
+    var promise = new Promise(function (resolve, reject) {
+        console.log('within helper')
+        var list = [];
+        groups.once('value')
+        .then(function(snapshot) {
+            console.log('here0')
+            snapshot.forEach(childSnapshot => {
+                console.log('here')
+                var item = childSnapshot.val();
+                console.log('here1')
+                console.log(JSON.parse(JSON.stringify(item)))
+                console.log('here2')
+                list.push(JSON.parse(JSON.stringify(item)));
+                console.log('here3')
+                return true
+            })
+            console.log('before resolve')
+            console.log(list)
+            resolve(list)
+            console.log('after resolve')
+        })
+        console.log('after foreach line 75')
+        console.log('line 76 list ' + list)
+        
+    });
+    return promise;
+}
+
+/*
+WITH .ON INSTEAD OF .ONCE
+export function helper(groups) {
+    var promise = new Promise(function (resolve, reject) {
         console.log('within helper')
         var list = [];
         groups.on('value', snapshot => {
@@ -76,15 +109,20 @@ export function helper(groups) {
                 console.log('here2')
                 list.push(JSON.parse(JSON.stringify(item)));
                 console.log('here3')
+                return true
             })
+            console.log('before resolve')
+            console.log(list)
+            resolve(list)
+            console.log('after resolve')
         })
         console.log('after foreach line 75')
         console.log('line 76 list ' + list)
-        resolve(list)
+        
     });
     return promise;
 }
-
+*/
 
 export const getUserWishlist = (userID) => {
     var groups = db.ref(`/users/${userID}/wishlist`);
